76eaa070e103c1aea7f458c2a0679ac36addc216
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2015-07-10 21:28:44 +0200

    Add intro to greets, better placement of stuff

--- src/ScreenLayer.js
+++ src/ScreenLayer.js
@@ -24,6 +24,7 @@ function ScreenLayer(layer) {
                                 }));
   this.scene.add(this.bg);
 
+  this.placementRandom = new Random('screenlayer, yo');
   var pixelGeometry = new THREE.BoxGeometry(16 * 0.95, 20 * 0.95, 1);
   for(var x = 0; x < 16; x++) {
     this.screen[x] = [];
@@ -39,16 +40,19 @@ function ScreenLayer(layer) {
       backPixel.position.z = -0.5;
       pixel.add(frontPixel);
       pixel.add(backPixel);
-      pixel.position.x = 16 * (x - 7.5);
-      pixel.position.y = 20 * (y - 4.5);
-      pixel.position.z = -100;
+      pixel.regularPosition = new THREE.Vector3();
+      pixel.regularPosition.x = 16 * (x - 7.5);
+      pixel.regularPosition.y = 20 * (y - 4.5);
+      pixel.regularPosition.z = -100;
+      pixel.startPosition = new THREE.Vector3();
+      pixel.startPosition.x = pixel.regularPosition.x * 3;
+      pixel.startPosition.y = pixel.regularPosition.y * 3;
+      pixel.startPosition.z = 100;
       pixel.targetRotation = new THREE.Vector3(0, 0, 0);
       this.screen[x][y] = pixel;
       this.scene.add(pixel);
     }
   }
-
-  this.camera.position.z = 200;
 
   this.renderPass = new THREE.RenderPass(this.scene, this.camera);
 
@@ -238,10 +242,21 @@ ScreenLayer.prototype.images = [
    '    |           ').split(''),
 ];
 
-ScreenLayer.prototype.update = function(frame) {
+ScreenLayer.prototype.update = function(frame, relativeFrame) {
   for(var x = 0; x < 16; x++) {
     for(var y = 0; y < 9; y++) {
       var pixel = this.screen[x][y];
+      var positioner = relativeFrame / 100;
+      pixel.position.x = smoothstep(pixel.startPosition.x,
+                                    pixel.regularPosition.x,
+                                    positioner);
+      pixel.position.y = smoothstep(pixel.startPosition.y,
+                                    pixel.regularPosition.y,
+                                    positioner);
+      pixel.position.z = smoothstep(pixel.startPosition.z,
+                                    pixel.regularPosition.z,
+                                    positioner);
+
       if(BEAN < 1272) {
         pixel.rotation.x = ((1 + x) * (1 + y) + frame / 20) % (Math.PI * 2);
       } else {
@@ -253,20 +268,21 @@ ScreenLayer.prototype.update = function(frame) {
   }
 
   this.camera.position.x = 0;
-  this.camera.position.z = 130;
+  this.camera.position.z = 230;
+  var xOffset = 30;
   if(BEAN >= 1272) {
     if(BEAN % 24 < 12) {
-      this.camera.position.x = -lerp(140, 120, (BEAN % 12) / 12);
-      this.camera.position.z = 100;
+      this.camera.position.x = +100 -lerp(140, 120, (BEAN % 12) / 12);
+      this.camera.position.z = 230;
     } else {
-      this.camera.position.x = lerp(140, 120, (BEAN % 12) / 12);
-      this.camera.position.z = 100;
+      this.camera.position.x = xOffset + lerp(140, 120, (BEAN % 12) / 12);
+      this.camera.position.z = 230;
     }
     if(BEAT && BEAN % 24 == 0) {
       this.camera.lookAt(new THREE.Vector3(-80, 0, 0));
     }
     if(BEAT && BEAN % 24 == 12) {
-      this.camera.lookAt(new THREE.Vector3(80, 0, 0));
+      this.camera.lookAt(new THREE.Vector3(0, 0, 0));
     }
   } else {
     this.camera.lookAt(new THREE.Vector3(0, 0, 0));
