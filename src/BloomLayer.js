ebbd41970d5753081037794b2827765297744d56
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2015-07-11 13:50:40 +0200

    Improve glows

--- src/BloomLayer.js
+++ src/BloomLayer.js
@@ -3,11 +3,20 @@
  */
 function BloomLayer(layer) {
   this.config = layer.config;
-  this.shaderPass = new THREE.BloomPass(1, 16, 160, 512);
+  this.shaderPass = new THREE.BloomPass(this.config.amount || 1, 16, 160, 512);
+  this.stab = 0;
 }
 
 BloomLayer.prototype.update = function(frame) {
-  this.shaderPass.copyUniforms.opacity.value = 1;
+
+  this.stab *= 0.9;
+  if(this.stab < 0.1) {
+    this.stab = 0;
+  }
+  if(BEAT && BEAN % 6 == 0) {
+    this.stab = 1;
+  }
+  this.shaderPass.copyUniforms.opacity.value = (this.config.amount || 1) * this.stab;
 };
 
 BloomLayer.prototype.getEffectComposerPass = function() {
