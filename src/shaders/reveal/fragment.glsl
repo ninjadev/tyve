bfb742053d90727ec6f40d4bca7968aa80eea8a3
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2015-07-11 19:55:30 +0200

    More alpha, fix shader style

--- src/shaders/reveal/fragment.glsl
+++ src/shaders/reveal/fragment.glsl
@@ -7,19 +7,21 @@ varying vec2 vUv;
 void main() {
     vec4 color = texture2D(texture, vUv);
 
-    gl_FragColor = color;
-    if(gl_FragColor.a > alphaBound){
-        gl_FragColor.a = alphaBound;
+    vec4 outp = color;
+    if(outp.a > alphaBound){
+        outp.a = alphaBound;
     }
 
     if(vUv.x > reveal && vUv.x < reveal + 0.05){
-       gl_FragColor.a *= 1. - ((vUv.x - reveal)/0.05);
-    }else if(vUv.x >= reveal + 0.05){
-       gl_FragColor.a = 0.;
+       outp.a *= 1. - ((vUv.x - reveal)/0.05);
+    }
+    else if(vUv.x >= reveal + 0.05){
+       outp.a = 0.5;
     }
 
     if(reveal == 0.){
-      gl_FragColor.a = 0.;
+      outp.a = 0.;
     }
 
+    gl_FragColor = outp;
 }
